e3735322b6bb1ed6d0174eed75c64ee02697ddc9
Rid-lin <vlad@vegner.org>
2020-01-28 10:49:11 +0900

    Clearing code

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -84,26 +84,6 @@ func (s *server) setRequestID(next http.Handler) http.Handler {
 	})
 }
 
-// func (s *server) logRequest(next http.Handler) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		logger := s.logger.WithFields(logrus.Fields{
-// 			"remote_addr": r.RemoteAddr,
-// 			"request_id":  r.Context().Value(ctxKeyRequestID),
-// 		})
-// 		logger.Infof("started %s %s", r.Method, r.RequestURI)
-
-// 		start := time.Now()
-// 		rw := &responseWriter{w, http.StatusOK}
-// 		next.ServeHTTP(rw, r)
-
-// 		logger.Infof(
-// 			"completed with %d %s in %v",
-// 			rw.code,
-// 			http.StatusText(rw.code),
-// 			time.Now().Sub(start))
-// 	})
-// }
-
 func (s *server) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		logger := s.logger.WithFields(logrus.Fields{
